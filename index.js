da2e0e5f180aef62f4022a1455ff59c07bea1664
timceharris <timceharris@gmail.com>
2022-06-07 15:49:15 +0100

    initial

---
+++ index.js
@@ -0,0 +1,13 @@
+const createSVG = (array = [[50,50],[50,100],[250,100],[250,50]],height = 150,width = 300) => {
+
+    let coordstring = ''
+
+    for (let i = 0; i < array.length; i++){
+        coordstring = coordstring + (array[i][0] + ',' + array[i][1] + ' ')
+    }
+
+    xml = '<svg version="1.1" width="' + width + '" height="' + height + '" xmlns="http://www.w3.org/2000/svg"> <polygon points="' + coordstring + '"> </polygon> </svg>'
+    return(xml)
+
+
+}
